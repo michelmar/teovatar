23ccf7ed510f0540b593a061187f47c81ab0f7be
michel <michelsenfar@gmail.com>
2021-11-06 18:42:52 -0300

    template running fine

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,13 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  res.statusCode = 200;
+  res.json({ name: "John Doe" });
+};
+
+function getData(req: NextApiRequest, res: NextApiResponse) {
+  res.statusCode = 200;
+  res.json({ name: "John Doe" });
+}
